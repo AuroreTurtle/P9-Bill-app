17a033236eb7ae0fc639f67bf489d5726c77b8de
Aurore GOURRAUD <aurore.gourraud@gmail.com>
2023-02-11 18:04:43 +0100

    Fix the bug with the image extension and add error message

--- Billed-app-FR-Front/src/containers/NewBill.js
+++ Billed-app-FR-Front/src/containers/NewBill.js
@@ -15,31 +15,47 @@ export default class NewBill {
     this.billId = null
     new Logout({ document, localStorage, onNavigate })
   }
-  handleChangeFile = e => {
-    e.preventDefault()
-    const file = this.document.querySelector(`input[data-testid="file"]`).files[0]
-    const filePath = e.target.value.split(/\\/g)
-    const fileName = filePath[filePath.length-1]
-    const formData = new FormData()
-    const email = JSON.parse(localStorage.getItem("user")).email
-    formData.append('file', file)
-    formData.append('email', email)
 
-    this.store
-      .bills()
-      .create({
-        data: formData,
-        headers: {
-          noContentType: true
-        }
-      })
-      .then(({fileUrl, key}) => {
-        console.log(fileUrl)
-        this.billId = key
-        this.fileUrl = fileUrl
-        this.fileName = fileName
-      }).catch(error => console.error(error))
+  handleChangeFile = e => {
+      e.preventDefault();
+      const messageError = document.querySelector(".error");
+      messageError.style.display = "none";
+      messageError.style.visibility = "hidden";
+      const file = this.document.querySelector(`input[data-testid="file"]`).files[0];
+
+      const formatAuthorized = ["image/jpg", "image/jpeg", "image/png"];
+      const formatFile = file.type;
+      const checkFormat = formatAuthorized.includes(formatFile);
+
+      if (checkFormat) {
+          const filePath = e.target.value.split(/\\/g);
+          const fileName = filePath[filePath.length - 1];
+          const formData = new FormData();
+          const email = JSON.parse(localStorage.getItem("user")).email;
+          formData.append("file", file);
+          formData.append("email", email);
+
+          this.store
+              .bills()
+              .create({
+                  data: formData,
+                  headers: {
+                      noContentType: true,
+                  },
+              })
+              .then(({ fileUrl, key }) => {
+                  console.log(fileUrl);
+                  this.billId = key;
+                  this.fileUrl = fileUrl;
+                  this.fileName = fileName;
+              })
+              .catch((error) => console.error(error));
+      } else {
+          messageError.style.display = "block";
+          messageError.style.visibility = "visible";
+      }
   }
+  
   handleSubmit = e => {
     e.preventDefault()
     console.log('e.target.querySelector(`input[data-testid="datepicker"]`).value', e.target.querySelector(`input[data-testid="datepicker"]`).value)

65b5d2bb84ad41558f0bf6644cd421ae80200a06
Aurore GOURRAUD <aurore.gourraud@gmail.com>
2023-02-17 13:43:07 +0100

    Fix the bug when consulting tickets

--- Billed-app-FR-Front/src/containers/Dashboard.js
+++ Billed-app-FR-Front/src/containers/Dashboard.js
@@ -145,12 +145,13 @@ export default class {
       this.counter ++
     }
 
-    bills.forEach(bill => {
-      $(`#open-bill${bill.id}`).click((e) => this.handleEditTicket(e, bill, bills))
-    })
+    bills.forEach((bill) => {
+        $(`#open-bill${bill.id}`)
+            .off("click")
+            .on("click", (e) => this.handleEditTicket(e, bill, bills));
+    });
 
-    return bills
-
+    return bills;
   }
 
   getBillsAllUsers = () => {

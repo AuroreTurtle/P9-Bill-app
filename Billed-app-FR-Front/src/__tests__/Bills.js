ffc1ff34f7d87ac56d3c5fd091771d4ce95045d8
Aurore GOURRAUD <aurore.gourraud@gmail.com>
2023-03-18 12:07:45 +0100

    Add integration tests

--- Billed-app-FR-Front/src/__tests__/Bills.js
+++ Billed-app-FR-Front/src/__tests__/Bills.js
@@ -3,13 +3,16 @@
  */
 
 import "@testing-library/jest-dom";
-import { screen, waitFor } from "@testing-library/dom";
+import { fireEvent, screen, waitFor } from "@testing-library/dom";
 import BillsUI from "../views/BillsUI.js";
 import { bills } from "../fixtures/bills.js";
-import { ROUTES_PATH } from "../constants/routes.js";
+import { ROUTES, ROUTES_PATH } from "../constants/routes.js";
 import { localStorageMock } from "../__mocks__/localStorage.js";
+import mockStore from "../__mocks__/store";
+import Bills from "../containers/Bills.js";
 
 import router from "../app/Router.js";
+jest.mock("../app/store", () => mockStore);
 
 describe("Given I am connected as an employee", () => {
     describe("When I am on Bills Page", () => {
@@ -40,6 +43,117 @@ describe("Given I am connected as an employee", () => {
             const antiChrono = (a, b) => a - b;
             const datesSorted = [...dates].sort(antiChrono);
             expect(dates).toEqual(datesSorted);
+        });
+
+        //Test handleClickIconEye
+        describe("When I click on eye icon", () => {
+            test("Then the modal open", () => {
+                // Simulation du stockage de données de l'utilisateur et de la connexion en tant qu'employé
+                Object.defineProperty(window, "localStorage", { value: localStorageMock });
+                window.localStorage.setItem("user", JSON.stringify({ type: "Employee" }));
+                const onNavigate = (pathname) => {
+                    document.body.innerHTML = ROUTES({ pathname });
+                };
+
+                // Affichage des notes de frais
+                document.body.innerHTML = BillsUI({ data: bills });
+                const billsContainer = new Bills({
+                    document,
+                    onNavigate,
+                    store: null,
+                    localStorage: window.localStorage,
+                });
+
+                //Affiche la modale
+                $.fn.modal = jest.fn();
+
+                // Création de la méthode handleClickIconEye
+                const mockHandleClickIconEye = jest.fn(() => {
+                    billsContainer.handleClickIconEye;
+                });
+                const iconEye = screen.getAllByTestId("icon-eye")[0];
+                iconEye.addEventListener("click", mockHandleClickIconEye);
+                // Simulation du clic
+                fireEvent.click(iconEye);
+                expect(mockHandleClickIconEye).toHaveBeenCalled();
+                expect($.fn.modal).toHaveBeenCalled();
+            });
+        });
+
+        // Test GET
+        describe("When I get bills", () => {
+            test("Then bills should be displayed", async () => {
+                localStorage.setItem("user", JSON.stringify({ type: "Employee", email: "a@a" }));
+                const root = document.createElement("div");
+                root.setAttribute("id", "root");
+                document.body.append(root);
+                router();
+                window.onNavigate(ROUTES_PATH.Bills);
+                await waitFor(() => screen.getByText("Mes notes de frais"));
+
+                //récupération des notes de frais dans le store
+                const billsStore = new Bills({
+                    document,
+                    onNavigate,
+                    store: mockStore,
+                    localStorage: window.localStorage,
+                });
+                const bills = await billsStore.getBills();
+                expect(bills.length).toBe(4);
+            });
+        });
+
+        // Test Error
+        describe("When an error occurs on API", () => {
+            // Initialisation des variables nécessaires avant les tests
+            beforeEach(() => {
+                // Espionne la méthode bills() du mockStore et définit un localStorage fictif pour simuler le stockage des données de l'utilisateur
+                jest.spyOn(mockStore, "bills");
+                Object.defineProperty(window, "localStorage", { value: localStorageMock });
+                window.localStorage.setItem(
+                    "user",
+                    JSON.stringify({
+                        type: "Employee",
+                        email: "a@a",
+                    })
+                );
+                const root = document.createElement("div");
+                root.setAttribute("id", "root");
+                document.body.appendChild(root);
+                router();
+            });
+
+            test("Then I fetch the bills in the API and fails with 404 message error", async () => {
+                // Simulation du cas où la méthode list() du mockStore échoue avec un message d'erreur 404
+                mockStore.bills.mockImplementationOnce(() => {
+                    return {
+                        list: () => {
+                            return Promise.reject(new Error("Erreur 404"));
+                        },
+                    };
+                });
+                window.onNavigate(ROUTES_PATH.Bills);
+                await new Promise(process.nextTick);
+                // Cherche le texte "Erreur 404"
+                const message = await screen.getByText(/Erreur 404/);
+                expect(message).toBeTruthy();
+            });
+
+            test("Then I fetch the bills in the API and fails with 500 message error", async () => {
+                // Simulation du cas où la méthode list() du mockStore échoue avec un message d'erreur 500
+                mockStore.bills.mockImplementationOnce(() => {
+                    return {
+                        list: () => {
+                            return Promise.reject(new Error("Erreur 500"));
+                        },
+                    };
+                });
+                window.onNavigate(ROUTES_PATH.Bills);
+                await new Promise(process.nextTick);
+                // Cherche le texte "Erreur 500"
+                const message = await screen.getByText(/Erreur 500/);
+                expect(message).toBeTruthy();
+            });
         });
     });
 });
--- Billed-app-FR-Front/src/__tests__/NewBill.js
+++ Billed-app-FR-Front/src/__tests__/NewBill.js
@@ -2,17 +2,176 @@
  * @jest-environment jsdom
  */
 
-import { screen } from "@testing-library/dom"
-import NewBillUI from "../views/NewBillUI.js"
-import NewBill from "../containers/NewBill.js"
+import "@testing-library/jest-dom";
+import { fireEvent, screen } from "@testing-library/dom";
+import { localStorageMock } from "../__mocks__/localStorage.js";
+import mockStore from "../__mocks__/store";
+import { ROUTES } from "../constants/routes.js";
+import BillsUI from "../views/BillsUI.js";
+import NewBillUI from "../views/NewBillUI.js";
+import NewBill from "../containers/NewBill.js";
 
+jest.mock("../app/store", () => mockStore);
 
 describe("Given I am connected as an employee", () => {
-  describe("When I am on NewBill Page", () => {
-    test("Then ...", () => {
-      const html = NewBillUI()
-      document.body.innerHTML = html
-      //to-do write assertion
-    })
-  })
-})
+    describe("When I am on NewBill Page", () => {
+        test('Then the text "Envoyer une note de frais" should be displayed', () => {
+            const html = NewBillUI();
+            document.body.innerHTML = html;
+            //to-do write assertion
+            expect(screen.getAllByText("Envoyer une note de frais")).toBeTruthy();
+        });
+    });
+
+    describe("When I download a file with an incorrect extension", () => {
+        test("Then an error should be displayed", () => {
+            // Simulation du stockage de données de l'utilisateur et de la connexion en tant qu'employé
+            Object.defineProperty(window, "localStorage", { value: localStorageMock });
+            window.localStorage.setItem("user", JSON.stringify({ type: "Employee" }));
+            const onNavigate = (pathname) => {
+                document.body.innerHTML = ROUTES({ pathname });
+            };
+
+            // Affichage de la page
+            document.body.innerHTML = NewBillUI();
+
+            const newBill = new NewBill({
+                document,
+                onNavigate,
+                store: null,
+                localStorage: window.localStorage,
+            });
+
+            // Récupération de l'input de type file
+            const inputFile = screen.getByTestId("file");
+            // Simulation du chargement d'un fichier avec une extension incorrecte
+            fireEvent.change(inputFile, {
+                target: {
+                    files: [new File(["image.gif"], "image.gif", { type: "image/gif" })],
+                },
+            });
+
+            const errorMessage = screen.getByTestId("error-message");
+            expect(errorMessage).toBeTruthy();
+        });
+    });
+
+    describe("When I download a file with an correct extension", () => {
+        test("Then the fiel is correct", () => {
+            Object.defineProperty(window, "localStorage", { value: localStorageMock });
+            window.localStorage.setItem("user", JSON.stringify({ type: "Employee" }));
+            const onNavigate = (pathname) => {
+                document.body.innerHTML = ROUTES({ pathname });
+            };
+
+            document.body.innerHTML = NewBillUI();
+
+            const newBill = new NewBill({
+                document,
+                onNavigate,
+                store: mockStore,
+                localStorage: window.localStorage,
+            });
+
+            const inputFile = screen.getByTestId("file");
+            // Simulation du chargement d'un fichier avec une extension correcte
+            fireEvent.change(inputFile, {
+                target: {
+                    files: [new File(["image.jpg"], "image.jpg", { type: "image/jpg" })],
+                },
+            });
+
+            const errorMessage = screen.getByTestId("error-message");
+            expect(errorMessage).toHaveStyle({ display: "none" });
+        });
+    });
+
+    //Test POST
+    describe("When I submit the form completed", () => {
+        test("Then the new bill is created", async () => {
+            // Simulation du stockage de données de l'utilisateur et de la connexion en tant qu'employé
+            Object.defineProperty(window, "localStorage", { value: localStorageMock });
+            window.localStorage.setItem("user", JSON.stringify({ type: "Employee" }));
+            const onNavigate = (pathname) => {
+                document.body.innerHTML = ROUTES({ pathname });
+            };
+
+            document.body.innerHTML = NewBillUI();
+            const newBillContainer = new NewBill({
+                document,
+                onNavigate,
+                store: null,
+                localStorage: window.localStorage,
+            });
+
+            // Simulation de la saisie des informations de la note de frais
+            const bill = {
+                type: "Transports",
+                name: "Train Paris-Nice",
+                date: "2023-02-02",
+                amount: 200,
+                vat: 50,
+                pct: 20,
+                commentary: "Facture du train Paris-Nice",
+                fileUrl: "../img/facture-train-2023_02_02.jpg",
+                fileName: "facture-train-2023_02_02.jpg",
+                status: "pending",
+            };
+
+            screen.getByTestId("expense-type").value = bill.type;
+            screen.getByTestId("expense-name").value = bill.name;
+            screen.getByTestId("datepicker").value = bill.date;
+            screen.getByTestId("amount").value = bill.amount;
+            screen.getByTestId("vat").value = bill.vat;
+            screen.getByTestId("pct").value = bill.pct;
+            screen.getByTestId("commentary").value = bill.commentary;
+            newBillContainer.fileName = bill.fileName;
+            newBillContainer.fileUrl = bill.fileUrl;
+
+            const mockHandleSubmit = jest.fn((e) => newBillContainer.handleSubmit(e));
+            const form = screen.getByTestId("form-new-bill");
+            form.addEventListener("submit", mockHandleSubmit);
+            fireEvent.submit(form);
+
+            expect(mockHandleSubmit).toHaveBeenCalled();
+        });
+
+        // Test Error
+        test("Then I fetch the bills in the API and fails with 404 message error", async () => {
+            // Espionne la méthode bills() du mockStore
+            jest.spyOn(mockStore, "bills");
+            // Simulation du cas où la méthode list() du mockStore échoue avec un message d'erreur 404
+            mockStore.bills.mockImplementationOnce(() => {
+                return {
+                    list: () => {
+                        return Promise.reject(new Error("Erreur 404"));
+                    },
+                };
+            });
+            const html = BillsUI({ error: "Erreur 404" });
+            document.body.innerHTML = html;
+            // Cherche le texte "Erreur 404"
+            const message = await screen.getByText(/Erreur 404/);
+            expect(message).toBeTruthy();
+        });
+
+        test("Then I fetch the bills in the API and fails with 500 message error", async () => {
+            // Espionne la méthode bills() du mockStore
+            jest.spyOn(mockStore, "bills");
+            // Simulation du cas où la méthode list() du mockStore échoue avec un message d'erreur 500
+            mockStore.bills.mockImplementationOnce(() => {
+                return {
+                    list: () => {
+                        return Promise.reject(new Error("Erreur 500"));
+                    },
+                };
+            });
+            const html = BillsUI({ error: "Erreur 500" });
+            document.body.innerHTML = html;
+            // Cherche le texte "Erreur 500"
+            const message = await screen.getByText(/Erreur 500/);
+            expect(message).toBeTruthy();
+        });
+    });
+});
+
